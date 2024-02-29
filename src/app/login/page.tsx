3893d86dbd5de88c7ec0fd76e0a1b6a3a0793f24
Ethan Tyler <38672607+zonedv2@users.noreply.github.com>
2024-02-29 18:32:36 +0000

    implemented basic auth usign lucia and github OAuth

---
+++ src/app/login/page.tsx
@@ -0,0 +1,16 @@
+import { validateRequest } from "../../auth";
+import { redirect } from "next/navigation";
+
+
+export default async function Page() {
+  const { user } = await validateRequest();
+	if (user) {
+		return redirect("/dashboard");
+  }
+	return (
+		<>
+			<h1>Sign in</h1>
+			<a href="/login/github">Sign in with GitHub</a>
+		</>
+	);
+}
