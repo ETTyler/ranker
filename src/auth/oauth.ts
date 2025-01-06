1a18f53e18e18b8dc1e9a5f1f1668b8ecb2e44e3
Ethan Tyler <38672607+zonedv2@users.noreply.github.com>
2025-01-06 02:15:17 +0000

    bug fix

--- src/auth/oauth.ts
+++ src/auth/oauth.ts
@@ -3,5 +3,5 @@ import { GitHub, Google } from "arctic";
 export const google = new Google(
   process.env.GOOGLE_CLIENT_ID!,
   process.env.GOOGLE_CLIENT_SECRET!,
-  process.env.GOOGLE_REDIRECT_URI!
+  process.env.GOOGLE_CALLBACK_URI!
 );
